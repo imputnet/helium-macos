23d45640d3f1dcdee58326ad50759f08870fcbe6
wukko <me@wukko.me>
2026-09-02 02:06:21 +0600

    sparkle: update to 2.9.6, build and use BinaryDelta directly
    sparkle cask is no longer available on homebrew, this fixes delta
    creation in release CI

--- .github/scripts/github_prep_sparkle_deltas.sh
+++ .github/scripts/github_prep_sparkle_deltas.sh
@@ -2,12 +2,10 @@
 set -ex
 
 pip3 install requests --break-system-packages
-brew install sparkle
-xattr -cr /opt/homebrew/Caskroom/sparkle/*/bin/BinaryDelta
-export PATH="$(echo /opt/homebrew/Caskroom/sparkle/*/bin):$PATH"
+export PATH="$PWD/build/src/out/Default:$PATH"
 
 python3 ./devutils/generate_sparkle_deltas.py "$@"
 
-echo 'deltas<<EOF' >> $GITHUB_OUTPUT
-find ./release_asset/ -name '*.delta' >> $GITHUB_OUTPUT
-echo EOF >> $GITHUB_OUTPUT
+echo 'deltas<<EOF' >> "$GITHUB_OUTPUT"
+find ./release_asset/ -name '*.delta' >> "$GITHUB_OUTPUT"
+echo EOF >> "$GITHUB_OUTPUT"
